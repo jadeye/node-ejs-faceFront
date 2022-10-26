19adfb81ce84cd8cee38c7ff4b6fe1d55a302ef5
yoni3593 <yoni263593@gmail.com>
2022-10-26 16:36:18 +0300

    create html form for script

---
+++ views/index.js
@@ -0,0 +1,17 @@
+// let from = document.forms["my_form"].value;
+// console.log(from);
+
+// let from1 = document.forms["my_form"].value;
+// console.log(from1);
+
+// document.forms[".valuemyForm"]["fname"]
+
+
+function validateForm(event) {
+   
+   let path = document.forms["my_form"]["avatar"].value;
+   let version_number = document.forms["my_form"]["lname"].value;
+   console.log("true");
+   console.log(path);
+   console.log(version_number);
+}
